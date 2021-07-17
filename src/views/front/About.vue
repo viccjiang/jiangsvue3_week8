97b03c6b5a36f906111cb7254d8ad6bfb8d02b2d
viccjiang <binbinbb520@gmail.com>
2021-07-17 15:41:22 +0800

    first commit

--- src/views/front/About.vue
+++ src/views/front/About.vue
@@ -1,18 +1,18 @@
 <template>
-<section>
-  <div id="carouselExampleCaptions" class="container carousel slide mt-5" data-bs-ride="carousel">
-    <div class="carousel-indicators"></div>
-    <div class="carousel-inner">
-      <div class="carousel-item active ">
-        <div
-          class="card-img-top"
-          style="height: 50vh; background-image: url(https://images.unsplash.com/photo-1519974719765-e6559eac2575?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=100);
+  <section>
+    <div id="carouselExampleCaptions" class="container carousel slide mt-5" data-bs-ride="carousel">
+      <div class="carousel-indicators"></div>
+      <div class="carousel-inner">
+        <div class="carousel-item active ">
+          <div
+            class="card-img-top"
+            style="height: 50vh; background-image: url(https://images.unsplash.com/photo-1519974719765-e6559eac2575?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=100);
           background-size: cover;
           background-position:center center;
           "
-        ></div>
-        <!-- <div class="carousel-caption d-none d-md-block ">
-          <h1>讓你的居家風格更加獨特</h1>
+          ></div>
+          <!-- <div class="carousel-caption d-none d-md-block ">
+          <h1>讓你的居家風格更加出色</h1>
           <p>帶你進入美好生活，創造你的生活美學</p>
 
           <router-link to="/products">
@@ -21,58 +21,58 @@
             </button>
           </router-link>
         </div> -->
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="about container">
-    <div class="contentText text-center my-5">
-      <h1>關於 RE：HOUSE</h1>
-      <p>
-        <br />
+    <div class="about container">
+      <div class="contentText text-center my-5">
+        <h1>關於 RE：HOUSE</h1>
+        <p>
+          <br />
 
-        成立於 2021 年，疫情打亂生活，<br />
-        <br />
-        決定開始長期的居家辦公生活，<br />
-        <br />
-        有了一個屬於自己的空間後，<br />
-        <br />
-        發現挑選傢俱、搭配風格是多麽療癒的事情，<br />
-        <br />
-        於是開始經營起自己的居家風格！<br />
-        <br />
-        也希望把這種經驗分享給，<br />
-        <br />
-        喜歡打造生活風格的你。<br />
-        <br />
-      </p>
+          成立於 2021 年，疫情打亂生活，<br />
+          <br />
+          決定開始長期的居家辦公生活，<br />
+          <br />
+          有了一個屬於自己的空間後，<br />
+          <br />
+          發現挑選傢俱、搭配風格是多麽療癒的事情，<br />
+          <br />
+          於是開始經營起自己的居家風格！<br />
+          <br />
+          也希望把這種經驗分享給，<br />
+          <br />
+          喜歡打造生活風格的你。<br />
+          <br />
+        </p>
 
-      <blockquote class="blockquote">
-        <p><mark> 我們將帶你進入美好生活，讓你享受每一刻精彩！ </mark></p>
-        <p><mark>一起為您打造美好居家生活 ，Make Life Easier！</mark></p>
-      </blockquote>
-    </div>
-  </div>
-
-  <!-- footer -->
-  <div class="bg-dark">
-    <div class="container">
-      <div class="d-flex align-items-center justify-content-between text-white py-4">
-        <p class="mb-0">© 2020 LOGO All Rights Reserved.</p>
-        <ul class="d-flex list-unstyled mb-0 h4">
-          <li>
-            <a href="#" class="text-white mx-3"><i class="fab fa-facebook"></i></a>
-          </li>
-          <li>
-            <a href="#" class="text-white mx-3"><i class="fab fa-instagram"></i></a>
-          </li>
-          <li>
-            <a href="#" class="text-white ms-3"><i class="fab fa-line"></i></a>
-          </li>
-        </ul>
+        <blockquote class="blockquote">
+          <p><mark> 我們將帶你進入美好生活，讓你享受每一刻精彩！ </mark></p>
+          <p><mark>一起為您打造美好居家生活 ，Make Life Easier！</mark></p>
+        </blockquote>
       </div>
     </div>
-  </div>
+
+    <!-- footer -->
+    <div class="bg-dark">
+      <div class="container">
+        <div class="d-flex align-items-center justify-content-between text-white py-4">
+          <p class="mb-0">© 2020 LOGO All Rights Reserved.</p>
+          <ul class="d-flex list-unstyled mb-0 h4">
+            <li>
+              <a href="#" class="text-white mx-3"><i class="fab fa-facebook"></i></a>
+            </li>
+            <li>
+              <a href="#" class="text-white mx-3"><i class="fab fa-instagram"></i></a>
+            </li>
+            <li>
+              <a href="#" class="text-white ms-3"><i class="fab fa-line"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
